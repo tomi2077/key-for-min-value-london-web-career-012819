6721677adc2524a5aa01f9ee1637cea223bbed14
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-26 14:23:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
+  if name_hash.length == 0
+    
   
   
   name_hash.each do |key, value|
