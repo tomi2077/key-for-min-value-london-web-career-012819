883c61ccdc97276f85705229f0da88f5595bd250
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-26 14:22:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  
+  
+  
   name_hash.each do |key, value|
     if value == name_hash.value.max 
       return key
