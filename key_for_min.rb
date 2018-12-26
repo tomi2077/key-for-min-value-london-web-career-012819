c86bd30814dbabdc0a60567e8201df176265825c
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-26 14:10:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,8 @@
 
 def key_for_min_value(name_hash)
   name_hash.each do |key, value|
-    if v ==  
+    if value == name_hash.value.max 
+      return 
 end
 
 hash.each { |k, v| puts k if v == hash.values.max }
