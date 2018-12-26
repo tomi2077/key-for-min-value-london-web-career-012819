fead1b5d75e366b3c5296892510c7cefe13c0dd2
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-26 14:23:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,14 +4,10 @@
 def key_for_min_value(name_hash)
   if name_hash.length == 0
     return nil
-  
-  
-  
+  else
   name_hash.each do |key, value|
     if value == name_hash.value.max 
       return key
-    elsif name_hash.empty?
-      return
     end
   end
 end
