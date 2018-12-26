ea759d1a8704f176581b10f0584f648187790165
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-26 14:20:15 +0000

    Automatically backed up by Learn

---
+++ key_for_min.rb
@@ -0,0 +1,12 @@
+# prereqs: iterators, hashes, conditional logic
+# Given a hash with numeric values, return the key for the smallest value
+
+def key_for_min_value(name_hash)
+  name_hash.each do |key, value|
+    if value == name_hash.value.max 
+      return key
+    elsif name_hash.empty?
+      return
+    end
+  end
+end
